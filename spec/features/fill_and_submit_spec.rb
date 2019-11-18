4e73f2f745480b0f781c72022ca87a6048be155a
Selina Erhabor <selinaerhabor@gmail.com>
2019-11-18 16:47:33 +0000

    Committing progress in challenges, set up

---
+++ spec/features/fill_and_submit_spec.rb
@@ -0,0 +1,10 @@
+# Expects players to fill in their names (in a form), submit that form and see those names on-screen
+feature 'Enter name, submit form, see name on screen' do
+  scenario 'submit names entered in form' do
+    visit('/')
+    fill_in :player_1_name, with: 'Sel'
+    fill_in :player_2_name, with: 'Gemini'
+    click_button 'Submit'
+    expect(page).to have_content 'Sel vs. Gemini'
+  end
+end
