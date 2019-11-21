58d0ad10dd7532a688dfd6531f137eb6b5b4c0d8
Selina Erhabor <selinaerhabor@gmail.com>
2019-11-21 15:27:22 +0000

    Defined Player class and initialize method and stated attr_reader as name'

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,10 +1,11 @@
-# require 'player'
+require 'player'
 
-# describe Player do
-#   subject(:shelton) { Player.new('Shelton') }
-# end
+describe Player do
+  subject(:shelton) { Player.new('Shelton') }
+
   describe '#name' do
     it 'returns the name' do
       expect(shelton.name).to eq 'Shelton'
     end
   end
+end
