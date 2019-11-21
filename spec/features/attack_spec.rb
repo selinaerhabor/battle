c54748b06f792a8b70efb43b7839786eafa6d4ce
Selina Erhabor <selinaerhabor@gmail.com>
2019-11-21 15:44:58 +0000

    Added feature test for scenario to reduce player 2 HP by 10

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,17 @@
 feature 'Attacking' do
+
   scenario 'attack Player 2' do
     sign_in_and_play
     click_link 'Attack'
     expect(page).to have_content 'Sel hit Gemini'
   end
+
+  scenario 'reduce player 2 HP by 10' do
+    sign_in_and_play
+    click_link 'Attack'
+    click_link 'OK'
+    expect(page).not_to have_content 'Gemini: 100HP'
+    expect(page).to have_content 'Gemini: 90HP'
+  end
+  
 end
